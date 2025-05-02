3ff9cc6cfbf0d25c672ee0085e7215bad4500e68
Manuel <000balderas@gmail.com>
2025-05-02 12:48:22 -0600

    Update panel admin

--- public/JS/admin.js
+++ public/JS/admin.js
@@ -1,15 +1,4 @@
-function escapeHTML(str) {
-    return str.replace(/[&<>"']/g, (m) =>
-      ({
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#39;',
-      })[m]
-    );
-  }
-  
+
   // Cargar exámenes
   fetch('/panel/examenesNombre')
     .then(res => res.json())
@@ -19,8 +8,8 @@ function escapeHTML(str) {
         const examenElement = document.createElement('div');
         examenElement.classList.add('examen');
         examenElement.innerHTML = `
-          <p><strong>Nombre:</strong> ${escapeHTML(examen.nombre)}</p>
-          <p><strong>Archivo:</strong> ${escapeHTML(examen.archivo)}</p>
+          <p><strong>Nombre:</strong> ${examen.nombre}</p>
+          <p><strong>Archivo:</strong> ${examen.archivo}</p>
           <button onclick="eliminarExamen(${index})">Eliminar</button>
           <hr>
         `;
@@ -50,10 +39,10 @@ function escapeHTML(str) {
         const messageElement = document.createElement('div');
         messageElement.classList.add('message');
         messageElement.innerHTML = `
-          <p><strong>Nombre:</strong> ${escapeHTML(mensaje.nombre)}</p>
-          <p><strong>Email:</strong> ${escapeHTML(mensaje.email)}</p>
-          <p><strong>Mensaje:</strong> ${escapeHTML(mensaje.mensaje)}</p>
-          <p><strong>Fecha:</strong> ${escapeHTML(mensaje.fecha)}</p>
+          <p><strong>Nombre:</strong> ${mensaje.nombre}</p>
+          <p><strong>Email:</strong> ${mensaje.email}</p>
+          <p><strong>Mensaje:</strong> ${mensaje.mensaje}</p>
+          <p><strong>Fecha:</strong> ${mensaje.fecha}</p>
           <button onclick="eliminarMensaje(${index})">Eliminar</button>
           <hr>
         `;
@@ -83,9 +72,9 @@ function escapeHTML(str) {
         const userElement = document.createElement('div');
         userElement.classList.add('user');
         userElement.innerHTML = `
-          <p><strong>Nombre:</strong> ${escapeHTML(usuario.nombre)}</p>
-          <p><strong>Email:</strong> ${escapeHTML(usuario.email)}</p>
-          <p><strong>Fecha de registro:</strong> ${escapeHTML(usuario.fecha)}</p>
+          <p><strong>Nombre:</strong> ${usuario.nombre}</p>
+          <p><strong>Email:</strong> ${usuario.email}</p>
+          <p><strong>Fecha de registro:</strong> ${usuario.fecha}</p>
           <button onclick="eliminarUsuario(${index})">Eliminar</button>
           <hr>
         `;
